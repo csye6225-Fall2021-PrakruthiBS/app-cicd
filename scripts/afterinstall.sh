f887c47b86aeeb249e8a3c199ffae749e675345b
PrakruthiPrasad <bagursuryanarayana.p@northeastern.edu>
2021-11-16 10:24:11 -0500

    chaged directories

--- scripts/afterinstall.sh
+++ scripts/afterinstall.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-cd /home/ubuntu/target
+cd /home/ubuntu/
 sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a fetch-config -m ec2 -c file:/home/ubuntu/CloudWatchAgentConfig.json -s
